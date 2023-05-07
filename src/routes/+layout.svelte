2bded3eb98fce929edbbf08901f3b6604c70295b
Rais Ilham <raispinter12@gmail.com>
2023-05-07 10:45:02 +0700

    add dropdown menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
-	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	import {
+		AppBar,
+		AppShell,
+		LightSwitch,
+		ListBox,
+		modalStore,
+		popup
+	} from '@skeletonlabs/skeleton';
+
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
 	// Your selected Skeleton theme:
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
@@ -9,13 +20,64 @@
 
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.postcss';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	let popupHover: PopupSettings = {
+		event: 'click',
+		target: 'popupHover',
+		placement: 'bottom'
+		// Close the popup when the item is clicked
+	};
 </script>
 
 <!-- App Shell -->
-<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
+<AppShell>
 	<svelte:fragment slot="header" />
 	<svelte:fragment slot="pageHeader">
-		<AppBar>AjakJago</AppBar>
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead"
+				><a href="/"
+					><div class="">
+						<span class="text-3xl">Ajak</span><span class="text-3xl font-semibold text-blue-700"
+							>Jago</span
+						>
+					</div></a
+				></svelte:fragment
+			>
+			<div>
+				<button class="btn hover:bg-blue-700 hover:text-white" use:popup={popupHover}>
+					<span>Dropdown menu</span>
+					<i class="fa-sharp fa-solid fa-caret-down" />
+				</button>
+				<div class="card p-4 w-60 shadow-xl" data-popup="popupHover">
+					<nav class="list-nav">
+						<ul>
+							<li>
+								<a href="/">
+									<span class="w-6 text-center"><i class="fa-solid fa-home" /></span>
+									<span>Homepage</span>
+								</a>
+							</li>
+							<li>
+								<a href="/docs/get-started">
+									<span class="w-6 text-center"><i class="fa-solid fa-book" /></span>
+									<span>Documentation</span>
+								</a>
+							</li>
+							<li>
+								<a href="/blog">
+									<span class="w-6 text-center"><i class="fa-solid fa-bullhorn" /></span>
+									<span>Blog</span>
+								</a>
+							</li>
+							<!-- <hr class="!my-4" /> -->
+						</ul>
+					</nav>
+					<div class="arrow bg-surface-100-800-token" />
+				</div>
+			</div>
+
+			<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+		</AppBar>
 	</svelte:fragment>
 	<!-- Router Slot -->
 	<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
-	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	import { AppBar, AppShell, ListBox, ListBoxItem, popup } from '@skeletonlabs/skeleton';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
+
+	let comboboxValue: string;
 </script>
 
 <AppShell>
 	<div class="flex items-center justify-center h-screen">
 		<div class="container p-8 space-y-8">
-			<h1>Hello Skeleton</h1>
+			<h1>Hello World</h1>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 			<section>
-				<a class="btn variant-filled-primary" href="https://kit.svelte.dev/"
-					>SvelteKit</a
-				>
-				<a class="btn variant-filled-secondary" href="https://tailwindcss.com/"
-					>Tailwind</a
-				>
-				<a class="btn variant-filled-tertiary" href="https://github.com/"
-					>GitHub</a
-				>
-				<LightSwitch class="mt-5" />
+				<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+				<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+				<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
 			</section>
 		</div>
 	</div>
